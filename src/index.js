3539ccf3fa101053a759310790f33df9df0303c5
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-03-08 20:34:24 +0400

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  if(arr[arr.length-1]>=6)return false;
+  let left = 0;
+  let right = 0;
+  let left2 = 0;
+  let right2 = 0;
+  let left3 = 0;
+  let right3 = 0;
+  let check = 1;
+  for(let i=0; i<arr.length; i++){
+  	if(arr[i]=='(' && arr[i+1]!=']' && (arr[i+1]!='|'  && arr[i+2]!=')'))left++;
+  	if(arr[i]=='(' && (arr[i+1]==']' || (arr[i+1]=='|'  && arr[i+2]==')')))return false;
+  	if(arr[i]==')' && right<left)right++;
+  	if(arr[i]=='[')left2++;
+  	if(arr[i]==']' && right2<left2)right2++;
+  	if(arr[i]=='|')left3++;
+  	if(arr[i]=='|' && right3<left3)right3++;
+  }
+  if(check){
+  let left = 0;
+  let right = 0;
+  let left2 = 0;
+  let right2 = 0;
+  let left3 = 0;
+  let right3 = 0;
+  for(let i=0; i<arr.length; i++){
+  	if(arr[i]=='(')left++;
+  	if(arr[i]==')')right++;
+  	if(arr[i]=='[')left2++;
+  	if(arr[i]==']')right2++;
+  	if(arr[i]=='|')left3++;
+  	if(arr[i]=='|')right3++;
+  }
+  check=1;
+  if((left+left2+left3)!=(right+right2+right3))
+  	return false;
+}
+  return ((left+left2+left3)==(right+right2+right3));
 }
