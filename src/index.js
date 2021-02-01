48ef77ab30329eba8e0459336f6e387ae9483bd2
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2021-02-01 09:48:44 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  let arr = str.split('');
-  if(arr[arr.length-1]>=6)return false;
-  let left = 0;
-  let right = 0;
-  let left2 = 0;
-  let right2 = 0;
-  let left3 = 0;
-  let right3 = 0;
-  let check = 1;
-  for(let i=0; i<arr.length; i++){
-  	if(arr[i]=='(' && arr[i+1]!=']' && (arr[i+1]!='|'  && arr[i+2]!=')'))left++;
-  	if(arr[i]=='(' && (arr[i+1]==']' || (arr[i+1]=='|'  && arr[i+2]==')')))return false;
-  	if(arr[i]==')' && right<left)right++;
-  	if(arr[i]=='[')left2++;
-  	if(arr[i]==']' && right2<left2)right2++;
-  	if(arr[i]=='|')left3++;
-  	if(arr[i]=='|' && right3<left3)right3++;
+function removeBrackets(str, typeBrackets) {
+  for (let i = 0; i < typeBrackets.length; i++) {
+    while (str.includes(typeBrackets[i])) {
+      str = str.replace(typeBrackets[i],'');
+    }
   }
-  if(check){
-  let left = 0;
-  let right = 0;
-  let left2 = 0;
-  let right2 = 0;
-  let left3 = 0;
-  let right3 = 0;
-  for(let i=0; i<arr.length; i++){
-  	if(arr[i]=='(')left++;
-  	if(arr[i]==')')right++;
-  	if(arr[i]=='[')left2++;
-  	if(arr[i]==']')right2++;
-  	if(arr[i]=='|')left3++;
-  	if(arr[i]=='|')right3++;
-  }
-  check=1;
-  if((left+left2+left3)!=(right+right2+right3))
-  	return false;
+  return str;
 }
-  return ((left+left2+left3)==(right+right2+right3));
+
+function checkBrackets(str, typeBrackets) {
+    let result = false;
+    for (let i = 0; i < typeBrackets.length; i++) {
+        if (str.includes(typeBrackets[i])) {
+            result = true;
+        }
+    }
+    return result;
+  }
+
+module.exports = function check(str, bracketsConfig) {
+  let typeBrackets = bracketsConfig.slice(0);
+  let result = true;
+  typeBrackets = typeBrackets.map((e) => `${e[0]}${e[1]}`);
+  while (result) {
+    str = removeBrackets(str, typeBrackets);
+    result = checkBrackets(str, typeBrackets);
+  }
+  return str.length === 0;
 }
